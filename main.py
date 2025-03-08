badf5c54558294cc5aa66b6e376a00f93d8c27fc
vipinbansal179 <bansalvipin36@gmail.com>
2025-03-09 02:23:59 +0530

    Add data validation stage and configuration updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -8,6 +9,18 @@ try:
     logger.info(f"Pipeline started for stage: {STAGE_NAME}")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"Pipeline ended for stage: {STAGE_NAME}")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "stage_02_data_validation"
+
+try:
+    logger.info(f"Pipeline started for stage: {STAGE_NAME}")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"Pipeline ended for stage: {STAGE_NAME}")
 except Exception as e:
     logger.exception(e)
